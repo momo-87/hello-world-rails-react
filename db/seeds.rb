2d2d08f34ee67a2fbd2f11ef3631841e1290dfba
momo-87 <momochristian87@gmail.com>
2023-09-18 20:44:23 +0100

    Create a table for storing  messages and create 5 different greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greetings = ['Hello Momo', 'Good Morning Momo', 'What up Momo']
+
+greetings.each do |item|
+  Greeting.create!(message: item)
+end
