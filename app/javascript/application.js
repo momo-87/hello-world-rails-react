d6c5fbe01899f8b862d6202979344855773b3b79
momo-87 <momochristian87@gmail.com>
2023-09-19 16:27:29 +0100

    Display the random greeting in the Greeting component.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,15 @@
 // Entry point for the build script in your package.json
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store'
 import App from './components/App';
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root'));
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
